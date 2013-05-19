ff0d4b7a53febd19a1da950b1c8c4f588e1bb9bf
acacheung <acacheung@gmail.com>
2013-05-19 14:00:40 -0400

    inserted model validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,6 @@
 class Blog < ActiveRecord::Base
   attr_accessible :author_id, :title, :body
+
+  validates :title, :presence => true
+  validates :body, :presence => true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :user_id, :body
+
+  validates :body, :presence => true
 end
